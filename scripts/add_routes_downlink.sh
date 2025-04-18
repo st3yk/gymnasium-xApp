e28fb0b162c0ed0ca766c717aec2d5647dd1ecdf
Tymon Szczepanowski <tymon.szczepanowski@gmail.com>
2025-04-18 14:07:22 +0200

    Change script for generating traffic

---
+++ scripts/add_routes_downlink.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+set -x
+echo "Adding route from the host to gNB and UEs via core"
+sudo ip ro add 10.45.0.0/16 via 10.53.1.2
+echo "test gNB ping"
+ping 10.45.1.1 -c 4
+echo "Adding route from from UEs to the host via core"
+for i in {1..3}; do
+    ip=$((i + 1))
+    sudo ip netns exec ue${i} ip route add default via 10.45.1.1 dev tun_srsue
+    ping 10.45.1.${ip} -c 4
+done
